6cd1c7cc21cbc00dfc871361c4231abc69646d92
Shinji Yamada <dotneet@gmail.com>
2022-12-17 14:10:28 +0900

    add graph impl

---
+++ src/lib/graph.spec.ts
@@ -0,0 +1,34 @@
+import test from "ava";
+
+import { createGraph, Graph } from "./graph";
+
+test("createGraph", (t) => {
+  const g: Graph = createGraph();
+  const v1 = g.createVertex("human", {
+    name: "Bob",
+  });
+  const v2 = g.createVertex("human", {
+    name: "Alice",
+  });
+  const edge = g.createEdge(v1, v2);
+  t.is(edge.inVertex.id, v1.id);
+  t.is(edge.outVertex.id, v2.id);
+  t.is(v1.props.get("name"), "Bob");
+  t.is(v2.props.get("name"), "Alice");
+  t.is(g.vertices.size, 2);
+
+  t.is(v1.outEdges.size, 1);
+  t.is(v1.outVertices().size, 1);
+
+  t.is(v2.inEdges.size, 1);
+  t.is(v2.inVertices().size, 1);
+
+  t.is(g.edges.size, 1);
+  g.deleteEdge(edge);
+  t.is(g.edges.size, 0);
+
+  t.is(g.vertices.size, 2);
+  g.deleteVertex(v1);
+  g.deleteVertex(v2);
+  t.is(g.vertices.size, 0);
+});
---
+++ src/lib/graph.ts
@@ -0,0 +1,118 @@
+import { v4 as uuidv4 } from "uuid";
+
+type PropsValue = string | number;
+type PropsParameter =
+  | Record<string, PropsValue>
+  | ReadonlyMap<string, PropsValue>;
+
+function obj2map(
+  objOrMap: PropsParameter,
+): Map<string, PropsValue> {
+  if (objOrMap instanceof Map) {
+    return obj2map as unknown as Map<string, PropsValue>;
+  } else if (objOrMap instanceof Object) {
+    return new Map(Object.entries(objOrMap));
+  } else {
+    return new Map();
+  }
+}
+
+export class Vertex {
+  constructor(
+    public readonly id: string,
+    public readonly label: string,
+    public props: Map<string, PropsValue>,
+    public inEdges: Set<Edge> = new Set(),
+    public outEdges: Set<Edge> = new Set(),
+  ) {}
+
+  outVertices(): Set<Vertex> {
+    const result: Set<Vertex> = new Set();
+    this.outEdges.forEach((e) => result.add(e.outVertex));
+    return result;
+  }
+
+  inVertices(): Set<Vertex> {
+    const result: Set<Vertex> = new Set();
+    this.inEdges.forEach((e) => result.add(e.outVertex));
+    return result;
+  }
+}
+
+export class Edge {
+  constructor(
+    public readonly id: string,
+    public inVertex: Vertex,
+    public outVertex: Vertex,
+    public props: Map<string, PropsValue>,
+  ) {}
+}
+
+export class Traversal {
+  readonly graph: Graph;
+  constructor(graph: Graph) {
+    this.graph = graph;
+  }
+}
+
+export class Graph {
+  vertices: Set<Vertex>;
+  edges: Set<Edge>;
+
+  constructor() {
+    this.vertices = new Set();
+    this.edges = new Set();
+  }
+
+  createVertex(
+    label: string,
+    props: PropsParameter = new Map(),
+  ): Vertex {
+    const v: Vertex = new Vertex(
+      uuidv4(),
+      label,
+      obj2map(props),
+    );
+    this.vertices.add(v);
+    return v;
+  }
+
+  createEdge(
+    inVertex: Vertex,
+    outVertex: Vertex,
+    props: PropsParameter = new Map(),
+  ): Edge {
+    const e: Edge = new Edge(
+      uuidv4(),
+      inVertex,
+      outVertex,
+      obj2map(props),
+    );
+    inVertex.outEdges.add(e);
+    outVertex.inEdges.add(e);
+    this.edges.add(e);
+    return e;
+  }
+
+  deleteVertex(vertex: Vertex): void {
+    vertex.inEdges.forEach((e) => this.edges.delete(e));
+    vertex.outEdges.forEach((e) => this.edges.delete(e));
+    this.vertices.delete(vertex);
+  }
+
+  deleteEdge(
+    edge: Edge,
+  ): void {
+    this.edges.delete(edge);
+    edge.inVertex.outEdges.delete(edge);
+    edge.outVertex.inEdges.delete(edge);
+  }
+
+  createTraversal(): Traversal {
+    return new Traversal(this);
+  }
+}
+
+export function createGraph(): Graph {
+  return new Graph();
+}
